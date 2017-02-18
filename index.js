faed8edc9cc2da9264d0496e51e837874121404d
Dieseleration <tjr11r@my.fsu.edu>
2017-02-18 04:26:13 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,12 @@
-/*!
- * express
- * Copyright(c) 2009-2013 TJ Holowaychuk
- * Copyright(c) 2013 Roman Shtylman
- * Copyright(c) 2014-2015 Douglas Christopher Wilson
- * MIT Licensed
- */
+const WebSocket = require('ws');
+const gdax = require('gdax');
 
-'use strict';
-
-module.exports = require('./lib/express');
-
-var gdax = require('gdax');	//gdax
-var http = require('http');	//http
-var socket = require('ws');	//websocket
-var fs = require('fs');		//filesystem ops
-
-
-var pub = new gdax.PublicClient(); //init client
-
-/*
-var getgdaxdata = function(err, response, data) {
-	console.log('called getgdaxdata function');
-	console.log(data);
-	
-};
-*/
-
-//Product Data Grabber callback
-var grabfunc = function(err, response, data) {
-	console.log('pinging GDAX');
-	console.log(data);
-}
-
-var hBTC = pub.getProductHistoricRates({'granularity': 50}, grabfunc);
-var ws = new socket('marquiseIP/socketnum');
-ws.on('open', function open() {
-	ws.send(pub.getProductHistoricRates{'granularity': 300}, grabfunc);
+const ws = new WebSocket('http://localhost:3003');
+ws.on('open', () => console.log('socket open'));
+const pub = new gdax.PublicClient();
+setInterval(() => {
+pub.getProductHistoricRates({ granularity: 100 }, (err, res, data) => {
+  ws.send(data);
+  console.log(data);
 });
-
+}, 1000);
