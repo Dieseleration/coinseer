ce6a05d9e789949760192be2470f1a10e35cf26f
Dieseleration <tjr11r@my.fsu.edu>
2017-02-18 02:29:52 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -10,27 +10,31 @@
 
 module.exports = require('./lib/express');
 
-var gdax = require('gdax');
-var http = require('http');
-//var fs = require('fs');
+var gdax = require('gdax');	//gdax
+var http = require('http');	//http
+var socket = require('ws');	//websocket
+var fs = require('fs');		//filesystem ops
 
-var pubclient = new gdax.PublicClient(); //init client
+
+var pub = new gdax.PublicClient(); //init client
+
+/*
 var getgdaxdata = function(err, response, data) {
 	console.log('called getgdaxdata function');
 	console.log(data);
+	
 };
+*/
 
-var output = pubclient.getProductTicker(getgdaxdata);
-//console.log(output);
+//Product Data Grabber callback
+var grabfunc = function(err, response, data) {
+	console.log('pinging GDAX');
+	console.log(data);
+}
 
-
-
-
-
-//fs.writeFile("/testfile.txt", output, function(err) {
-	//console.log('file saved');
-//});
-//console.log(data);
-
-
+var hBTC = pub.getProductHistoricRates({'granularity': 50}, grabfunc);
+var ws = new socket('marquiseIP/socketnum');
+ws.on('open', function open() {
+	ws.send(pub.getProductHistoricRates{'granularity': 300}, grabfunc);
+});
 
